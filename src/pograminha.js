5529e4e7a3a0337b984b336a15a5110a5fbe7934
Daniel Victor <daniel.silva@apinteractive.com.br>
2021-04-07 19:40:17 -0300

    teste

--- src/pograminha.js
+++ src/pograminha.js
@@ -1,6 +1,6 @@
 const { listAllSetups, createNewSetup, dropSetup } = require('./service/getSetupService.js')
 
 listAllSetups()
-console.log(createNewSetup({id:4, owner:'Kauã', processor:'AMD Athlon XP 1500+', ram:'S'}))
+console.log(createNewSetup({id:4, owner:'Bruno', processor:'AMD Athlon XP 1500+', ram:'S'}))
 dropSetup(2)
 listAllSetups()
